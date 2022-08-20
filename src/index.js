9e195603842534fb89d6bff732ea24dbbece6b6f
Raul <raulehernandeza@gmail.com>
2022-08-20 07:23:48 -0400

    verificar heroku

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const app = express()
 
+if (process.env.NODE_ENV != 'production'){
+    require('dotenv').config()
+}
+
+//Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(require('./routes/routes'))
 
-process.env.PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.get('/',function(req,res){
     console.log("hola")
     res.sendStatus(200);
 })
 
-process.env.NODE_ENV = process.env.NODE_ENV || 3000;
-app.listen(process.env.PORT, ()=> {
-    console.log("Escuchando en puerto 3000");
+app.listen(PORT, ()=> {
+    console.log("Escuchando en puerto")
+    console.log(PORT)
 })
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express.Router()
-//app.use('/login',require('./login'))
-//app.use('/registro',require('./registro'))
+
 app.use('/menu',require('../hello/hello'))
-//app.use('/form',require('./form'))
+
 
 module.exports = app
