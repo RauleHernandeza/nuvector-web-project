e27ea8fd30fb48e14eba6e098a4675cbb6173693
Raul <raulehernandeza@gmail.com>
2022-08-23 03:04:11 -0400

    what could be done

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -3,26 +3,24 @@ const { Client } = require('pg');
 const getusers = async (req, res) => {
 
     const info = req.body
-    login(info).then(resp=>{
-        console.log(resp)
-        res.sendFile('/home/raul/Escritorio/Node/nuvector_web_project/src/views/board.html')
+    login(info).then(resp => {
 
-        //res.redirect('https://www.youtube.com/')
-        
-    }).catch(err=>{
-      console.log("error "+err)
-      res.send({status:408,body:err});
+        console.log(resp)
+        res.redirect('https://nuvector-web-project-front.herokuapp.com/board.html')
+
+    }).catch(err => {
+        console.log("error " + err)
+        res.send({ status: 408, body: err });
     })
 
 
 }
 
-const login= async (info)=>{
-    const client = new Client({connectionString:process.env.URL_DB})
+const login = async (info) => {
+    const client = new Client({ connectionString: process.env.URL_DB })
     client.connect()
-    let h = await client.query('SELECT * FROM users where email = $1 AND password =$2' ,[info.email , info.password])
+    let h = await client.query('SELECT * FROM users where email = $1 AND password =$2', [info.email, info.password])
     client.end()
-    console.log(h)
     return h
 }
 
